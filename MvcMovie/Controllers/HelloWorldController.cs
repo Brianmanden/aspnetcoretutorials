04a4312b3e7f08e86e8d609265cb28103f1107e6
Brianmanden <brianmanden@gmail.com>
2020-04-20 07:23:51 +0200

    Add controller + clean up.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int numTimes = 1) {
+            //return "This is the Welcome action method...";
+            //var output = "";
+
+            //for (int i = 1; i <= numTimes; i++)
+            //{
+            //    output += $"Hello {name} - progress: {i} of {numTimes}";
+            //}
+
+            return HtmlEncoder.Default.Encode( $"Hello {name}: numTimes is {numTimes}");
+            //return HtmlEncoder.Default.Encode(output);
+        }
+    }
+}
